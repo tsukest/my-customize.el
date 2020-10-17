6149f723aa39a5748c2910a3cb264e72f7578cf1
satake <satake.ts@gmail.com>
2020-10-17 12:38:36 +0900

    add my-customize.el

---
+++ my-customize.el
@@ -0,0 +1,29 @@
+;;; my-customize.el -- My customize.el  -*- lexical-binding: t; -*-
+
+;; Author: satake <satake.ts@gmail.com>
+;; Maintainer: satake <satake.ts@gmail.com>
+;; URL: https://github.com/tsukest/my-customize.el
+;; Version: 0.0.1
+;; Keywords: my
+;; Package-Requires: ((emacs "24.5") (ivy "0.13.0"))
+
+;;; Commentary:
+
+;; This is my setting
+
+;;; Code:
+
+(require 'ivy)
+
+(defun mycustomize-select-shell ()
+  "Select shell to run."
+  (interactive)
+  (ivy-read "Use: " '("shell-command"
+                      "eshell-command")
+            :preselect "^shell-command"
+            :action (lambda (user-input)
+                      (counsel-M-x-action user-input))
+            :caller 'mycustomize-select-shell))
+
+(provide 'my-customize)
+;;; my-customize.el ends here
